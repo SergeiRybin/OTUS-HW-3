9eed37aafc76cb9f7fd410ab1a571ee1fb0a7917
Sergei Rybin <Sergei.Rybin@softeq.com>
2023-09-17 00:48:09 +0300

    Added multiple envvars to bash test

--- test/scripts/out_equality_test.sh
+++ test/scripts/out_equality_test.sh
@@ -3,11 +3,10 @@
 TEMP_FOLDER=test-tmp
 
 # get out of build dir
-cd ..
 echo $CMAKE_BUILD_TYPE > $TEMP_FOLDER/log.txt
 [ -d $TEMP_FOLDER ] || mkdir $TEMP_FOLDER
-cat ./test/data/ip_filter.tsv | ./build/$CMAKE_BUILD_TYPE/ipfilter > ./$TEMP_FOLDER/out.txt
-diff ./test/data/ip_filter.tst ./$TEMP_FOLDER/out.txt > ./$TEMP_FOLDER/diff.txt
+cat $CMAKE_CURRENT_SOURCE_DIR/test/data/ip_filter.tsv | $CMAKE_CURRENT_BINARY_DIR/ipfilter > ./$TEMP_FOLDER/out.txt
+diff $CMAKE_CURRENT_SOURCE_DIR/test/data/ip_filter.tst ./$TEMP_FOLDER/out.txt > ./$TEMP_FOLDER/diff.txt
 STATUS_CODE=$?
 
 if [[ $STATUS_CODE -eq 0 ]]
